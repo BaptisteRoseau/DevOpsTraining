ce0b8cd4d5f5d5f1d2936062be28a06ce5a87e07
Shynamo <broseau@gmail.com>
2023-06-17 10:53:00 +0200

    Remove gitlab-runner user fom script as it is installed before

--- utils/install_gitlab_runner.sh
+++ utils/install_gitlab_runner.sh
@@ -3,6 +3,3 @@ arch=$(dpkg --print-architecture)
 curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_${arch}.deb"
 sudo dpkg -i gitlab-runner_${arch}.deb
 sudo gitlab-runner start
-sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
-sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
-sudo gitlab-runner start
