3b5f6542f6bac8cc47bb9e60d8975e0ed660738f
Shynamo <broseau@gmail.com>
2023-07-06 22:58:31 +0200

    Add CRUD implementation tests

--- application/src/routes.rs
+++ application/src/routes.rs
@@ -1,16 +1,17 @@
-use crate::implementation::{create_item, delete_item, get_item, update_item};
+use crate::implementation::{create_item, delete_item, read_item, update_item};
 use crate::models::Database;
 use uuid::Uuid;
 use warp::{Filter, Rejection, Reply};
 
-// Define the routes for CRUD operations
+/// Defines the routes for CRUD operations
+#[allow(opaque_hidden_inferred_bound)]
 pub(crate) fn routes(
     database: Database,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     let get_item_filter = warp::path!("items" / Uuid)
         .and(warp::get())
         .and(with_database(database.clone()))
-        .and_then(get_item);
+        .and_then(read_item);
 
     let create_item_filter = warp::path!("items")
         .and(warp::post())
@@ -35,7 +36,7 @@ pub(crate) fn routes(
         .or(delete_item_filter)
 }
 
-// Helper function to pass the database as a parameter
+/// Helper function to pass the database as a parameter
 fn with_database(
     database: Database,
 ) -> impl Filter<Extract = (Database,), Error = std::convert::Infallible> + Clone {
